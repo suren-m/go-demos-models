e65c4eeb48356fdb7c078bc9dcf46288c0acd22e
suren-m <suren2535@hotmail.com>
2021-12-01 13:48:38 +0000

    Update album.go

--- music/album.go
+++ music/album.go
@@ -6,3 +6,12 @@ type Album struct {
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
 }
+
+
+type AlbumV2 struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+	Rating int     `json:"rating"`
+}
